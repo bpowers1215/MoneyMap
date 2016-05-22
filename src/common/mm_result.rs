c5864cee1d7b2de4cef63bc2762b3e9c9743e6b5
Brandon Powers <bpowers1215@gmail.com>
2016-05-22 18:09:55 -0400

    Add custom error handling

---
+++ src/common/mm_result.rs
@@ -0,0 +1,33 @@
+// src/common/mm_result.rs
+//MM Result type
+
+use std::fmt;
+
+pub type MMResult<T> = Result<T, MMError>;
+
+#[derive(Debug)]
+pub struct MMError{
+    kind:MMErrorKind,
+    message: String
+}
+
+#[derive(Debug)]
+pub enum MMErrorKind{
+    Database,
+    Api,
+    Other
+}
+
+impl MMError{
+    pub fn new(msg: String, kind: MMErrorKind) -> MMError{
+        MMError{
+            kind:kind,
+            message:msg
+        }
+    }
+}
+impl fmt::Display for MMError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
