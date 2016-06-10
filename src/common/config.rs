4692308e70cecab61ab280f7d2176b75f0d967ab
Brandon Powers <bpowers1215@gmail.com>
2016-06-09 21:29:56 -0400

    Config: move database parsing to separate function

--- src/common/config.rs
+++ src/common/config.rs
@@ -34,108 +34,17 @@ impl Config{
         let mut database = None;
 
         match read_config_from_file("config/config.toml"){
-            Ok(configString) => {
+            Ok(config_string) => {
                 //debug!("Config String: {}", configString);
-                match toml::Parser::new(&configString).parse(){
-                    Some(configTable) => {
-                        match configTable.get("database"){
-                            Some(dc) => {
-                                match dc.as_table(){
-                                    Some(databaseConfig) => {
-                                        //std::collections::BTreeMap
-                                        let host = match databaseConfig.get("host"){
-                                            Some(v) => {
-                                                //v: toml::Value
-                                                match v.as_str(){
-                                                    Some(vs) => Some(vs.to_string()),
-                                                    None => {
-                                                        warn!("Cannot read database host as string");
-                                                        None
-                                                    }
-                                                }
-                                            },
-                                            None => {
-                                                warn!("Database host not found in configuration");
-                                                None
-                                            }
-                                        };
-                                        let port = match databaseConfig.get("port"){
-                                            Some(v) => v.as_integer(),//v: toml::Value
-                                            None => {
-                                                warn!("Database port not found in configuration");
-                                                None
-                                            }
-                                        };
-                                        let name = match databaseConfig.get("name"){
-                                            Some(v) => {
-                                                //v: toml::Value
-                                                match v.as_str(){
-                                                    Some(vs) => Some(vs.to_string()),
-                                                    None => {
-                                                        warn!("Cannot read database name as string");
-                                                        None
-                                                    }
-                                                }
-                                            },
-                                            None => {
-                                                warn!("Database name not found in configuration");
-                                                None
-                                            }
-                                        };
-                                        let username = match databaseConfig.get("username"){
-                                            Some(v) => {
-                                                //v: toml::Value
-                                                match v.as_str(){
-                                                    Some(vs) => Some(vs.to_string()),
-                                                    None => {
-                                                        warn!("Cannot read database username as string");
-                                                        None
-                                                    }
-                                                }
-                                            },
-                                            None => {
-                                                warn!("Database username not found in configuration");
-                                                None
-                                            }
-                                        };
-                                        let password = match databaseConfig.get("password"){
-                                            Some(v) => {
-                                                //v: toml::Value
-                                                match v.as_str(){
-                                                    Some(vs) => Some(vs.to_string()),
-                                                    None => {
-                                                        warn!("Cannot read database password as string");
-                                                        None
-                                                    }
-                                                }
-                                            },
-                                            None => {
-                                                warn!("Database password not found in configuration");
-                                                None
-                                            }
-                                        };
-
-                                        database = Some(Database{
-                                            host: host,
-                                            port:  port,
-                                            name:  name,
-                                            username:  username,
-                                            password:  password
-                                        });
-                                    },
-                                    None => {}
-                                }
-                            },
-                            None => {
-                                warn!("No `database` configuration found.");
-                            }
-                        }
+                match toml::Parser::new(&config_string).parse(){
+                    Some(config_table) => {
+                        database = parse_database_config(config_table);
                     }
                     None => {}
                 }
             },
             Err(e) => {
-                warn!("Error reading configuration file");
+                warn!("{}", e);
             }
         }
 
@@ -170,4 +79,107 @@ fn read_config_from_file(file: &'static str) -> Result<String, io::Error>{
     try!(f.read_to_string(&mut buffer));
 
     Ok(buffer)
+}
+
+/// Parse database configuration
+///
+/// # Arguments
+/// * `config_table` - toml::Table The toml file represented as a BTreeMap
+///
+/// # Returns
+/// `Option<Database>` - Database Config
+fn parse_database_config(config_table: toml::Table) -> Option<Database>{
+    match config_table.get("database"){
+        Some(dc) => {
+            match dc.as_table(){
+                Some(database_config) => {
+                    //std::collections::BTreeMap
+                    let host = match database_config.get("host"){
+                        Some(v) => {
+                            //v: toml::Value
+                            match v.as_str(){
+                                Some(vs) => Some(vs.to_string()),
+                                None => {
+                                    warn!("Cannot read database host as string");
+                                    None
+                                }
+                            }
+                        },
+                        None => {
+                            warn!("Database host not found in configuration");
+                            None
+                        }
+                    };
+                    let port = match database_config.get("port"){
+                        Some(v) => v.as_integer(),//v: toml::Value
+                        None => {
+                            warn!("Database port not found in configuration");
+                            None
+                        }
+                    };
+                    let name = match database_config.get("name"){
+                        Some(v) => {
+                            //v: toml::Value
+                            match v.as_str(){
+                                Some(vs) => Some(vs.to_string()),
+                                None => {
+                                    warn!("Cannot read database name as string");
+                                    None
+                                }
+                            }
+                        },
+                        None => {
+                            warn!("Database name not found in configuration");
+                            None
+                        }
+                    };
+                    let username = match database_config.get("username"){
+                        Some(v) => {
+                            //v: toml::Value
+                            match v.as_str(){
+                                Some(vs) => Some(vs.to_string()),
+                                None => {
+                                    warn!("Cannot read database username as string");
+                                    None
+                                }
+                            }
+                        },
+                        None => {
+                            warn!("Database username not found in configuration");
+                            None
+                        }
+                    };
+                    let password = match database_config.get("password"){
+                        Some(v) => {
+                            //v: toml::Value
+                            match v.as_str(){
+                                Some(vs) => Some(vs.to_string()),
+                                None => {
+                                    warn!("Cannot read database password as string");
+                                    None
+                                }
+                            }
+                        },
+                        None => {
+                            warn!("Database password not found in configuration");
+                            None
+                        }
+                    };
+
+                    Some(Database{
+                        host: host,
+                        port:  port,
+                        name:  name,
+                        username:  username,
+                        password:  password
+                    })
+                },
+                None => None
+            }
+        },
+        None => {
+            warn!("No `database` configuration found.");
+            None
+        }
+    }
 }
