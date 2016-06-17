19b08999439d78e62160d4798d2263fcb058e493
Brandon Powers <brandon@exclamationlabs.com>
2016-06-16 21:54:44 -0400

    Create edit user for save

--- src/controllers/users.rs
+++ src/controllers/users.rs
@@ -8,7 +8,7 @@ use ::common::mm_result::{MMResult, MMError, MMErrorKind};
 use ::common::database::DB;
 
 //Models
-use ::models::user::{NewUser, PubUser};
+use ::models::user::{User, EditUser};
 
 // Nickel
 //use nickel::{JsonBody, Request, Response};
@@ -23,13 +23,21 @@ use nickel::{Nickel, JsonBody, HttpRouter, Request, Response, MiddlewareResult, 
 /// # Returns
 /// `String` - JSON String response
 pub fn create_user(req: &mut Request) -> String{
-    info!("Create User");
-    let new_user = req.json_as::<NewUser>().unwrap();
+    info!("Create New User");
+    let new_user = req.json_as::<EditUser>().unwrap();
     
-    // Serialize using `json::encode`
-    let encoded = json::encode(&new_user).unwrap();
-    match new_user.first_name{
-        Some(name) => format!(r#"{{"status":"success", "data":{{"user":{}}}}}"#, encoded),
-        None => format!(r#"{{"status":"success", "msg":"no first name"}}"#)
+    //Save User
+    let user = new_user.save();
+    
+    //Return Saved User
+    match user{
+        Ok(u) => {
+            let response = json::encode(&u).unwrap();
+            format!(r#"{{"status":"success", "data":{{"user":{}}}}}"#, response)
+        },
+        Err(_) => {
+            format!(r#"{{"status":"error", "msg":"Could not create user""#)
+        }
     }
+    
 }
--- src/models/user.rs
+++ src/models/user.rs
@@ -15,15 +15,17 @@ use ::common::database::DB;
 //use nickel::{JsonBody, Request, Response};
 use nickel::{Nickel, JsonBody, HttpRouter, Request, Response, MiddlewareResult, MediaType};
 
-/// Represent a User - visible data
-pub struct PubUser{
-    first_name: String,
-    last_name: String,
-    email: String
+/// User
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct User {
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub email: Option<String>
 }
 
+/// Edit User
 #[derive(RustcDecodable, RustcEncodable)]
-pub struct NewUser {
+pub struct EditUser {
     pub first_name: Option<String>,
     pub last_name: Option<String>,
     pub email: Option<String>,
@@ -31,68 +33,29 @@ pub struct NewUser {
     pub confirm_password: Option<String>
 }
 
-impl PubUser{
-
-    /// Create a PubUser
-    ///
-    /// # Returns
-    /// `PubUser`
-    pub fn new(first_name: String, last_name: String, email: String) -> PubUser{
-        PubUser{
-            first_name: first_name,
-            last_name: last_name,
-            email: email
-        }
-    }
-
-    /// Get First Name
-    ///
-    /// # Returns
-    /// `String` - first name
-    pub fn get_first_name(self) -> String{
-        self.first_name
-    }
-
-    /// Get Last Name
-    ///
-    /// # Returns
-    /// `String` - last name
-    pub fn get_last_name(self) -> String{
-        self.last_name
-    }
-
-    /// Get Name
-    ///
-    /// # Returns
-    /// `String` - full name
-    pub fn get_name(self) -> String{
-        format!("{} {}", self.first_name, self.last_name)
-    }
+// User Methods
+impl User{
 
 }
 
-/// Create New User
-///
-/// # Arguments
-///
-/// # Returns
-/// `MMResult<()>`
-pub fn create_user(req: &mut Request) -> String{
-    info!("Create User");
-    let new_user = req.json_as::<NewUser>().unwrap();
+// EditUser Methods
+impl EditUser{
     
-    // Serialize using `json::encode`
-    let encoded = json::encode(&new_user).unwrap();
-    match new_user.first_name{
-        Some(name) => format!(r#"{{"status":"success", "data":{}}}"#, encoded),
-        None => format!(r#"{{"status":"success", "msg":"no first name"}}"#)
+    /// Save User
+    ///
+    /// # Returns
+    /// 'MMResult<User>' - the saved user
+    pub fn save(self) -> MMResult<User>{
+        //validate user
+        
+        //save user to database
+        
+        //remove sensitive data and return new User struct
+        Ok(User{
+            first_name: self.first_name,
+            last_name: self.last_name,
+            email: self.email
+        })
     }
-}
 
-/*pub fn page<'mw, 'conn> (req: &mut Request<'mw, 'conn>, res: &mut Response<'mw>) -> String{
-    info!("PAGE ACCESSED!");
-    let new_user = req.json_as::<NewUser>().unwrap();
-    debug!("NAME: {}", new_user.first_name);
-    res.set(MediaType::Json);
-    format!("{{\"status\":\"success\", \"msg\":\"Welcome to Money Map!\"}}")
-}*/
+}
