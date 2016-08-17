e70c767307903d30ad7a009ea1d29ba5ff99a1f9
Brandon Powers <bpowers1215@gmail.com>
2016-08-16 21:14:29 -0400

    Cleanup warnings

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ extern crate chrono;
 extern crate bson;
 extern crate mongodb;
 
-// Todo Crate
+// Money Map Crate
 extern crate money_map;
 
 //Logging
@@ -22,27 +22,15 @@ extern crate log;
 extern crate log4rs;
 
 // Nickel
-use nickel::{Nickel, JsonBody, HttpRouter, Request, Response, MiddlewareResult, MediaType};
-use nickel::status::StatusCode::{self, Forbidden};
-
-// bson
-use bson::{Bson, Document};
-use bson::oid::ObjectId;
-
-// rustc_serialize
-use rustc_serialize::json::{Json, ToJson};
-use rustc_serialize::base64;
-use rustc_serialize::base64::{FromBase64};
+use nickel::{Nickel, HttpRouter, MediaType};
 
 //Common Utilities
 use money_map::common::database::DB as DB;
 use money_map::common::config::Config as Config;
-use money_map::common::api_result::{ApiResult, JsonEncoder};
+use money_map::common::api_result::{JsonEncoder};
 
 //DAO
 use money_map::dao::dao_manager::{DAOManager};
-use money_map::dao::user_dao::UserDAO;
-use money_map::dao::money_map_dao::MoneyMapDAO;
 
 //Controllers
 use money_map::controllers::controller_manager::ControllerManager;
@@ -78,7 +66,7 @@ fn main() {
     let mut server = Nickel::with_data(controller_manager);
     let mut router = Nickel::router();
 
-    router.get("/", middleware! { |request, mut response|
+    router.get("/", middleware! { |_request, mut response|
         info!("API Endpoint: /");
         response.set(MediaType::Json);
         format!("{{\"status\":\"success\", \"msg\":\"Welcome to Money Map!\"}}")
@@ -113,7 +101,7 @@ fn main() {
     });
 
     //Users Actions
-    router.get("/users", middleware! { |request, mut response|
+    router.get("/users", middleware! { |_request, mut response|
         info!("API Endpoint: GET /users");
         response.set(MediaType::Json);
         //let user = Controllers::users::PubUser::new("John".to_string(), "Smith".to_string(), "test@test.com".to_string());
