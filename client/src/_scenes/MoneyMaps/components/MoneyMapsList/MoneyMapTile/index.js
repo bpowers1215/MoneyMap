6bbf11feba71c2c8b29c47a30dceff7a4feaffee
Brandon Powers <bpowers1215@gmail.com>
2018-05-23 21:59:06 -0400

    Remove Link import

--- client/src/_scenes/MoneyMaps/components/MoneyMapsList/MoneyMapTile/index.js
+++ client/src/_scenes/MoneyMaps/components/MoneyMapsList/MoneyMapTile/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom'
 import AppLink from '~/_components/appLink';
 import './styles.scss';
 
